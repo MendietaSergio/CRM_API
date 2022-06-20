8918ddbe8302672647ad88cc18f81308c87fb497
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-20 13:07:16 -0300

    Añade controlador para guardar el nuevo cliente

--- Backend/controllers/clientsController.js
+++ Backend/controllers/clientsController.js
@@ -1,5 +1,17 @@
 const Clients= require('../models/Clients')
 
-exports.newClient = async (req,res) =>{
-    console.log(req.body);
+exports.newClient = async (req,res,next) =>{
+    // console.log(req.body);
+    const client = new Clients (req.body)
+
+    try {
+        await client.save()
+        res.json({
+            message: "Se guardó el cliente nuevo"
+        })
+    } catch (error) {
+        console.log(error);
+
+        next()
+    }
 }
