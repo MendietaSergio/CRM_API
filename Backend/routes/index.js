999364a2d9f182ea2454da2e0f72e7d816978ce9
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-20 17:42:18 -0300

    añade ruta para poder eliminar el producto

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,28 +1,38 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 //Controlladores
-const {newClient,
-        Clients,
-        idClients,
-        updateClient,
-        deleteClient
-} = require('../controllers/clientsController')
-const {newProduct, subirArchivo
-} = require('../controllers/productsController')
+const {
+  newClient,
+  Clients,
+  idClients,
+  updateClient,
+  deleteClient,
+} = require("../controllers/clientsController");
+const {
+  newProduct,
+  subirArchivo,
+  getProducts,
+  detailProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productsController");
 
-module.exports = function() {
-    
-    router.get('/clientes', Clients)
-    //agrega nuevos clientes
-    router.post('/clientes', newClient)
-    router.get('/clientes/:id', idClients)
+module.exports = function () {
+  router.get("/clientes", Clients);
+  //agrega nuevos clientes
+  router.post("/clientes", newClient);
+  router.get("/clientes/:id", idClients);
 
-    router.put('/clientes/:idClient', updateClient)
-    router.delete('/clientes/:idClient', deleteClient)
+  router.put("/clientes/:idClient", updateClient);
+  router.delete("/clientes/:idClient", deleteClient);
 
-    //PRODUCTOS
-    router.post('/productos', subirArchivo, newProduct)
+  //PRODUCTOS
+  router.get("/productos", getProducts);
+  router.post("/productos", subirArchivo, newProduct);
+  router.get("/productos/:idProduct", detailProduct);
+  router.put("/productos/:idProduct", subirArchivo, updateProduct);
+  router.delete("/productos/idProduct", deleteProduct); 
 
-    return router;
-}
+  return router;
+};
