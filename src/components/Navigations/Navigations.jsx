c4cf74a85015dfb3a57fb4b3b68347d5ad86de90
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-20 12:29:25 -0300

    añade mejoras en navigations

--- src/components/Navigations/Navigations.jsx
+++ src/components/Navigations/Navigations.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navigations = () => {
     return (
         <aside className='sidebar col-3'>
             <h2>Administración</h2>
             <nav className='navigation'>
-                <a href="" className="clients">Clientes</a>
-                <a href="" className="products">Productos</a>
-                <a href="" className="pedidos">Pedidos</a>
+                <Link to={"Clientes"} className="clients">Clientes</Link>
+                <Link to={"/Productos"} className="products">Productos</Link>
+                <Link to={"/Pedidos"} className="pedidos">Pedidos</Link>
             </nav>
         </aside>
     )
