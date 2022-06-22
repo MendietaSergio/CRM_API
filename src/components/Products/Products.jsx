aa66528ad6642be8f4c156435376f2bf86827bec
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-22 17:06:22 -0300

    Añade conexion con api productos, spinner, map de product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+import clientsAxios from '../../config/config'
+import { Spinner } from '../Spinner/Spinner'
+import { Product } from './Product'
+
 
 export const Products = () => {
+    
+    const [ products, setProducts] = useState([])
+
+    useEffect(() =>{
+        const getAPI = async () =>{
+            const { data } = await clientsAxios.get('/productos')
+            setProducts(data)
+        }
+        getAPI()
+    },[products])
+    
+    if(!products.length) return <Spinner />
+
     return (
+        <>
         <h2>Productos</h2>
+        <Link to={"/productos/nuevo"}>
+            <i className="fas fa-plus-circle"></i>
+            Nuevo producto
+        </Link>
+        <ul className="list-products" >
+            {products.map(product =>(
+                <Product 
+                    key={product._id}
+                    product={product}
+                    /> 
+            ))}
+        </ul>
+        </>
+
     )
 }
