0a281b611a2ef08a58144fc444ea830247d1f644
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-22 09:32:30 -0300

    Añade btn para agregar nuevo cliente

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import clientsAxios from "../../config/config";
 import { Client } from "./Client";
+import { Link } from 'react-router-dom'
 
 export const Clients = () => {
   const [clients, setClients] = useState([]);
@@ -21,13 +22,17 @@ export const Clients = () => {
   return (
     <>
       <h2>Clientes</h2>
-      {/* {loading ? (<p>cargando</p>) : ( */}
+      <Link to={"/clientes/nuevo"} className="btn btn-verde nvo-client">
+        <i className="fas fa-plus-circle"></i>
+        Nuevo Cliente
+      </Link>
+
         <ul className="list-clients">
           {clients.map((client) => (
             <Client key={client._id} client={client} />
           ))}
         </ul>
-      {/* )} */}
+
     </>
   );
 };
