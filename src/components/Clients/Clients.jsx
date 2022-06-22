7460199e1d123283d3452d3e9cedc679a81098b5
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-22 14:06:07 -0300

    Añade link, agrega loading para mostrar lista de clientes

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
 import clientsAxios from "../../config/config";
 import { Client } from "./Client";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export const Clients = () => {
   const [clients, setClients] = useState([]);
-//   const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getApi = async () => {
       const clientConsult = await clientsAxios.get("/clientes");
       console.log(clientConsult);
       setClients(clientConsult.data);
-    //   setTimeout(() => {
-        
-    //       setLoading(false);
-    //   }, 3000);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
     };
     getApi();
   }, []);
@@ -26,13 +25,13 @@ export const Clients = () => {
         <i className="fas fa-plus-circle"></i>
         Nuevo Cliente
       </Link>
-
+      {loading ? null : (
         <ul className="list-clients">
           {clients.map((client) => (
             <Client key={client._id} client={client} />
           ))}
         </ul>
-
+      )}
     </>
   );
 };
