f8bde7149eff7399e684674330a18c562bb6e08d
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-21 23:15:18 -0300

    Añade consumi de api para mostra lista de clientes

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import clientsAxios from "../../config/config";
+import { Client } from "./Client";
 
 export const Clients = () => {
-    return (
-        <h2>Clientes</h2>
-    )
-}
+  const [clients, setClients] = useState([]);
+//   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getApi = async () => {
+      const clientConsult = await clientsAxios.get("/clientes");
+      console.log(clientConsult);
+      setClients(clientConsult.data);
+    //   setTimeout(() => {
+        
+    //       setLoading(false);
+    //   }, 3000);
+    };
+    getApi();
+  }, []);
+
+  return (
+    <>
+      <h2>Clientes</h2>
+      {/* {loading ? (<p>cargando</p>) : ( */}
+        <ul className="list-clients">
+          {clients.map((client) => (
+            <Client key={client._id} client={client} />
+          ))}
+        </ul>
+      {/* )} */}
+    </>
+  );
+};
