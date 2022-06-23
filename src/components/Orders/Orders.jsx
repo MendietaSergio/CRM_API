def617253d377f2208fd7df2785dd0a7659ac827
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-23 12:45:07 -0300

    añade conexion con api, se toma info del pedido del cliente

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import clientsAxios from '../../config/config'
+import { DetailOrders } from './DetailOrders'
+
 
 export const Orders = () => {
+
+    const [ orders, setOrders] = useState([])
+
+    useEffect(() =>{
+        const getAPI = async () =>{
+            const result = await clientsAxios.get('/pedidos')
+            setOrders(result.data)
+        }
+        getAPI()
+    },[])
+
     return (
+        <>
         <h2>Pedidos</h2>
+        <ul className="list-orders">
+            {orders.map(order =>(
+            <DetailOrders
+                order={order}
+                key={order._id}
+            />
+            ))}
+        </ul>
+        </>
     )
 }
