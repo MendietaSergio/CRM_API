09dd6bb1f233dd126dd7e36865a66dc761c09c3a
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-23 12:44:04 -0300

    Añade componente detailorders, donde se mapea la info de la orden del cliente

---
+++ src/components/Orders/DetailOrders.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const DetailOrders = ({ order }) => {
+  const { client, orders, total } = order;
+
+  return (
+    <li className="order">
+      <div className="info-order">
+        <p className="id">ID:{order._id} </p>
+        <p className="name">
+          Cliente: {client.name} {client.surname}
+        </p>
+        <div className="article-order">
+          <p className="products">Artículo pedido: </p>
+          <ul>
+            {orders.map((product) => (
+              <li key={order._id + product.product._id}>
+                <p>{product.product.name}</p>
+                <p>Precio: {product.product.price}</p>
+                <p>Cantidad: {product.cant}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <p className="total">
+          Total: <span>$ {total}</span>{" "}
+        </p>
+      </div>
+      <div className="actions"></div>
+    </li>
+  );
+};
