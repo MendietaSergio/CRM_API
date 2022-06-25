235272770b9767e57b835b46d0adc5bcdd2b7e49
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-25 00:01:43 -0300

    Añade return para logear si no esta logeado, agrega token para conexion con api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,40 +1,55 @@
-import React,{useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
-import clientsAxios from '../../config/config'
-import { Spinner } from '../Spinner/Spinner'
-import { Product } from './Product'
+import React, { useEffect, useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import clientsAxios from "../../config/config";
+import { Spinner } from "../Spinner/Spinner";
+import { Product } from "./Product";
 
-
+import { CRMContext } from "../../Context/CRMContext";
 export const Products = () => {
-    
-    const [ products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [auth, setAuth] = useContext(CRMContext);
+  const navigate = useNavigate()
 
-    useEffect(() =>{
-        const getAPI = async () =>{
-            const { data } = await clientsAxios.get('/productos')
-            setProducts(data)
-        }
-        getAPI()
-    },[products])
-    
-    if(!products.length) return <Spinner />
+  useEffect(() => {
+    if (!auth.auth) return navigate("/clientes");
+  }, [])
 
-    return (
-        <>
-        <h2>Productos</h2>
-        <Link to={"/productos/nuevo"}>
-            <i className="fas fa-plus-circle"></i>
-            Nuevo producto
-        </Link>
-        <ul className="list-products" >
-            {products.map(product =>(
-                <Product 
-                    key={product._id}
-                    product={product}
-                    /> 
-            ))}
-        </ul>
-        </>
+  useEffect(() => {
+    if (auth.token !== "") {
+      try {
+        const getAPI = async () => {
+          const { data } = await clientsAxios.get("/productos",{
+            headers:{
+                Authorization: `Bearer ${auth.token}`
+              }
+          });
+          setProducts(data);
+        };
+        getAPI();
+      } catch (error) {
+        if(error.response.status(500)){
+            navigate("/clientes");
+          }
+      }
+    } else {
+        navigate("/iniciar-sesion");
+      }
+  }, [products]);
 
-    )
-}
+  if (!products.length) return <Spinner />;
+
+  return (
+    <>
+      <h2>Productos</h2>
+      <Link to={"/productos/nuevo"}>
+        <i className="fas fa-plus-circle"></i>
+        Nuevo producto
+      </Link>
+      <ul className="list-products">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </ul>
+    </>
+  );
+};
