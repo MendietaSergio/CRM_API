08e2bde3cdd72e89cbb9c29a7e5575e3e7cf57de
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-25 01:19:17 -0300

    Añade headers para poder eliminar cliente

--- src/components/Clients/Client.jsx
+++ src/components/Clients/Client.jsx
@@ -1,59 +1,60 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import clientsAxios from '../../config/config'
-export const Client = ({client}) => {
-    const {_id ,name, surname, business, email, phone} = client
-
-    const deleteClient = (_id) =>{
-        Swal.fire({
-            title: '¿Estás seguro?',
-            text: "Un cliente eliminado no se puede recuperar",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Si, eliminar!',
-            cancelButtonText: 'Cancelar'
-          }).then((result) => {
-            if (result.isConfirmed) {
-                console.log("eliminado... ",_id);
-                clientsAxios.delete(`/clientes/${_id}`)
-                .then((res) => {
-                    console.log(res)
-                      Swal.fire(
-                            'Eliminado!',
-                            res.data.message,
-                            'success'
-                          )
-                    })
-            }
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import clientsAxios from "../../config/config";
+import { CRMContext } from "../../Context/CRMContext";
+export const Client = ({ client }) => {
+  const { _id, name, surname, business, email, phone } = client;
+  const [auth, setAuth] = useContext(CRMContext);
+  const deleteClient = (_id) => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Un cliente eliminado no se puede recuperar",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clientsAxios
+          .delete(`/clientes/${_id}`, {
+            headers: {
+              Authorization: `Bearer ${auth.token}`,
+            },
           })
-    }
-    return (
-        <li className='client'>
-           <div className="info-client">
-                <p className="name">{name} {surname}</p>
-                <p className="business">{business}</p>
-                <p>{email}</p>
-                <p>Tel: {phone}</p>
-           </div>
-           <div className="actions">
-            
-            <Link to={`/clientes/editar/${_id}`} className="btn btn-azul">
-                <i className="fas fa-pen-alt"></i>Editar Cliente
-            </Link>
-            <Link to={`/pedidos/nuevo/${_id}`} className="btn btn-amarillo">
-                <i className="fas fa-plus"></i>Nuevo pedido
-            </Link>
-            <button 
-                className='btn btn-rojo'
-                type='button'
-                onClick={() =>deleteClient(_id)}
-                >
-                <i className="fas fa-times"></i>
-                Eliminar Cliente
-            </button>
-           </div>
-        </li>
-    )
-}
+          .then((res) => {
+            Swal.fire("Eliminado!", res.data.message, "success");
+          });
+      }
+    });
+  };
+  return (
+    <li className="client">
+      <div className="info-client">
+        <p className="name">
+          {name} {surname}
+        </p>
+        <p className="business">{business}</p>
+        <p>{email}</p>
+        <p>Tel: {phone}</p>
+      </div>
+      <div className="actions">
+        <Link to={`/clientes/editar/${_id}`} className="btn btn-azul">
+          <i className="fas fa-pen-alt"></i>Editar Cliente
+        </Link>
+        <Link to={`/pedidos/nuevo/${_id}`} className="btn btn-amarillo">
+          <i className="fas fa-plus"></i>Nuevo pedido
+        </Link>
+        <button
+          className="btn btn-rojo"
+          type="button"
+          onClick={() => deleteClient(_id)}
+        >
+          <i className="fas fa-times"></i>
+          Eliminar Cliente
+        </button>
+      </div>
+    </li>
+  );
+};
