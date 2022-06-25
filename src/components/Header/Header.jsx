a252279f48a0374de07da20af2e0bda68e149533
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-24 23:57:24 -0300

    añade btn para cerra sesion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CRMContext } from "../../Context/CRMContext";
 
 export const Header = () => {
-    return (
-        <header>
-            <div className="bar">
-                <div className="container">
-                    <h1>CRM - Administrador de Clientes</h1>
-                </div>
-            </div>
-        </header>
-    )
-}
+  const [auth, setAuth] = useContext(CRMContext);
+  const navigate = useNavigate()
+  const closedSesion =() =>{
+    setAuth({
+        token: '',
+        auth: false
+    })
+    localStorage.setItem('token', '')
+    navigate('/iniciar-sesion')
+  }
+  return (
+    <header>
+      <div className="bar">
+        <div className="container">
+          <div className="container-bar">
+            <h1>CRM - Administrador de Clientes</h1>
+          </div>
+          {auth.auth ? (
+            <button type="button"
+                    className="btn btn-rojo"
+                    onClick={closedSesion}
+                    >
+              <i className="fas fa-times-circle"></i>
+              Cerrar Sesión
+            </button>
+          ) : (
+            null
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
