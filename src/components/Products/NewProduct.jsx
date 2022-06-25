f13fca3679d056a2f5231af6ddd3e1444f9da9db
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-25 00:07:06 -0300

    añade redireccion a iniciar-sesion, si auth es false

--- src/components/Products/NewProduct.jsx
+++ src/components/Products/NewProduct.jsx
@@ -1,63 +1,63 @@
-import React, {useState, useEffect} from 'react'
-import clientsAxios from '../../config/config'
+import React, { useState, useEffect, useContext } from "react";
+import clientsAxios from "../../config/config";
 import { useNavigate } from "react-router-dom";
+import { CRMContext } from "../../Context/CRMContext";
 
 export const NewProduct = () => {
-    const navigate = useNavigate();
+  const [auth, setAuth] = useContext(CRMContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!auth.auth) return navigate("/iniciar-sesion");
+  }, []);
+  const [products, setProducts] = useState({
+    name: "",
+    price: "",
+  });
+  const [archive, setArchive] = useState("");
 
-    const [ products, setProducts ] = useState({
-        name:'',
-        price:''
-    })
-    const [archive, setArchive] = useState('')
+  const readArchive = (e) => {
+    console.log(e.target.files);
+    setArchive(e.target.files[0]);
+  };
 
-    const readArchive = (e) =>{
-        console.log(e.target.files);
-        setArchive(e.target.files[0])
+  const updateState = (e) => {
+    setProducts({
+      ...products,
+      [e.target.name]: [e.target.value],
+    });
+  };
+  const addProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", products.name);
+    formData.append("price", products.price);
+    formData.append("img", archive);
+
+    try {
+      const res = await clientsAxios.post("/productos", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (res.status === 200) {
+        Swal.fire("Agregado Correctamente", res.data.message, "success").then(
+          () => {
+            navigate("/productos");
+          }
+        );
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Hubo un error",
+        text: "Vuelva a intentarlo",
+      });
     }
+  };
 
-    const updateState = (e ) =>{
-        setProducts({
-            ...products,
-            [e.target.name] : [e.target.value]
-        })
-    }
-    const addProduct = async (e ) =>{
-        e.preventDefault()
-        const formData = new FormData()
-        formData.append('name', products.name)
-        formData.append('price', products.price)
-        formData.append('img', archive)
-
-        try {
-            const res = await clientsAxios.post('/productos', formData,{
-                headers:{
-                    'Content-Type' : 'multipart/form-data'
-                }
-            })
-            if(res.status===200){
-                Swal.fire(
-                    'Agregado Correctamente',
-                    res.data.message,
-                    'success'
-                ).then(
-                    () => {
-                      navigate("/productos");
-                    }
-                  );
-            }
-        } catch (error) {
-            Swal.fire({
-                icon:'error',
-                title: 'Hubo un error',
-                text:'Vuelva a intentarlo'
-            })
-        }
-
-    }
-    return (
-        <>
-          <h2>Nuevo Producto</h2>
+  return (
+    <>
+      <h2>Nuevo Producto</h2>
       <form onSubmit={addProduct}>
         <legend>Llena todos los campos</legend>
         <div className="field">
@@ -83,11 +83,7 @@ export const NewProduct = () => {
         </div>
         <div className="field">
           <label>Imagen: </label>
-          <input
-            type="file"
-            name="img"
-            onChange={readArchive}
-          />
+          <input type="file" name="img" onChange={readArchive} />
         </div>
         <div className="send">
           <input
@@ -97,7 +93,7 @@ export const NewProduct = () => {
             // disabled={validateClient()}
           />
         </div>
-      </form>  
-        </>
-    )
-}
+      </form>
+    </>
+  );
+};
