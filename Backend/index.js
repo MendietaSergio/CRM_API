64da2315f82aa58392e22b3a0711f6030c8ed584
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-26 16:09:26 -0300

    añade config. cors

--- Backend/index.js
+++ Backend/index.js
@@ -42,7 +42,12 @@ mongoose
 
 //rutas de la app
 app.use("/", routes());
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 app.listen(port, host, () =>{
