094c653ebd87c9d23f474e4f52d24396464f7b32
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-26 17:56:50 -0300

    Añade  path

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const app = express();
+const path = require('path')
 require('dotenv').config()
 
 const cors = require('cors')
